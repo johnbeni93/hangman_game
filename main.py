b99d3ba32831da5c1d466a50881ef1e090f89ce7
johnbeni93 <johnbeni93@gmail.com>
2020-11-14 22:47:55 +0000

    Letters disappear and update on word section

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ for i in range(7):
 #game variables
 hangman_status = 0
 word = "ELEMENTARY"
-guessed = ["E", "L"]
+guessed = []
 
 
 #colors
@@ -91,5 +91,6 @@ while run:
                 dis = math.sqrt((x - m_x)**2 + (y - m_y)**2)
                 if dis < RADIUS:
                   letter[3] = False
+                  guessed.append(ltr)
 
 pygame.quit()
